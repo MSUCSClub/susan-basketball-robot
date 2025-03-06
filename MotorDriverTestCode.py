4a678577f7bf4f536f1a73822d0a76ada18f6abb
hhicks1113 <126603177+hhicks1113@users.noreply.github.com>
2025-03-06 12:59:36 -0600

    Added pin numbers for all sensors and motors. Changed Drive functions with new motor driver inputs Updated functions with new drive functions and sensor functionality

---
+++ MotorDriverTestCode.py
@@ -0,0 +1,75 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+#GPIO Set Up#
+GPIO.setmode(GPIO.BCM)  #BCM uses the GPIO numbering on pinouts, not the actual pin number sometimes printed on boards.
+# Motor 1 (M1)
+M1enL = 23  # Set pins to where they are wired. Can change this value here instead of everywhere it is referenced.
+M1enR = 24
+M1pwmL = 13
+M1pwmR = 14
+# Motor 2 (M2)
+M2enL = 23
+M2enR = 24
+M2pwmL = 13
+M2pwmR = 14
+# Motor 1 (M1)
+GPIO.setup(M1enL,GPIO.OUT)  # Setting those pins as output pins so the board knows to output signal there.
+GPIO.setup(M1enR,GPIO.OUT)
+GPIO.setup(M1pwmL,GPIO.OUT)
+GPIO.setup(M1pwmR,GPIO.OUT)
+# Motor 2 (M2)
+GPIO.setup(M2enL,GPIO.OUT)
+GPIO.setup(M2enR,GPIO.OUT)
+GPIO.setup(M2pwmL,GPIO.OUT)
+GPIO.setup(M2pwmR,GPIO.OUT)
+# Set up the PWM pins so the board will send PWM signals.
+M1pwmL = GPIO.PWM(M1pwmL,1000)  #1000 is the Hz frequency. We can maybe play with this value to change the motor speed.
+M1pwmR = GPIO.PWM(M1pwmR,1000)
+M2pwmL = GPIO.PWM(M2pwmL,1000)
+M2pwmR = GPIO.PWM(M2pwmR,1000)  #NOTES ON CONTROLLING MOTORS WITH PWM SIGNALS
+                                    #pwmL.start(100) will start the motor spinning left at 100% speed
+                                    #pwmL.stop() will stop the motor
+                                    #pwmL.ChangeDutyCycle(50) will set the speed of the motor to 50%
+
+def driveLeft(speed):   #I'm not actually sure if this is left so might need to change the name once we test on Sue.
+    GPIO.output(M1enL,GPIO.HIGH)
+    GPIO.output(M1enR,GPIO.HIGH)
+    GPIO.output(M2enL,GPIO.HIGH)
+    GPIO.output(M2enR,GPIO.HIGH)
+    M1pwmL.start(speed)  #starts at a speed of whatever % int you call the function with.
+    M2pwmL.start(speed)
+    print(f"Going Left at {speed}% speed")
+
+def driveRight(speed): #I'm not actually sure if this is Right so might need to change the name once we test on Sue.
+    GPIO.output(M1enL,GPIO.HIGH)
+    GPIO.output(M1enR,GPIO.HIGH)
+    GPIO.output(M2enL,GPIO.HIGH)
+    GPIO.output(M2enR,GPIO.HIGH)
+    M1pwmR.start(speed)   #starts at a speed of whatever % int you call the function with.
+    M2pwmR.start(speed)
+    print(f"Going Right at {speed}% speed")
+
+def driveStop():
+    M1pwmL.stop()
+    M1pwmR.stop()
+    M2pwmL.stop()
+    M2pwmR.stop()
+    print("Stopped")
+
+try:
+    print("Starting test.")
+
+    driveLeft(50)
+    sleep(2)
+    driveStop()
+    sleep(2)
+    driveRight(75)
+    sleep(2)
+    driveStop()
+
+    print("Test Complete.")
+
+except KeyboardInterrupt:
+    print("Keyboard Interrupt")
+    GPIO.cleanup()
--- NewSue.py
+++ NewSue.py
@@ -3,66 +3,94 @@
 import time
 import RPi.GPIO as GPIO
 import board
-#import adafruit_tca9548a
-#import adafruit_vl53l0x
-#import adafruit_apds9960.apds9960
-from gpiozero import Button, DistanceSensor
-from Raspi_MotorHAT_master import Raspi_MotorHAT, Raspi_DCMotor
+from gpiozero import Button, DistanceSensor, LineSensor
 
-####### NEEDS VALUE CHECKED FOR PINS #######
+####### NEEDS VALUE CHECKED FOR PINS AFTER WIRING #######
+######## DRIVE MOTOR SET UP ##########
+# Drive Motor 1 (M1)
+M1enL = 5  # Set pins to where they are wired. Can change this value here instead of everywhere it is referenced.
+M1enR = 6
+M1pwmL = 12
+M1pwmR = 13
+# Drive Motor 2 (M2)
+M2enL = 26
+M2enR = 16
+M2pwmL = 18
+M2pwmR = 19
+# Drive Motor 1 (M1)
+GPIO.setup(M1enL,GPIO.OUT)  # Setting those pins as output pins so the board knows to output signal there.
+GPIO.setup(M1enR,GPIO.OUT)
+GPIO.setup(M1pwmL,GPIO.OUT)
+GPIO.setup(M1pwmR,GPIO.OUT)
+# Drive Motor 2 (M2)
+GPIO.setup(M2enL,GPIO.OUT)
+GPIO.setup(M2enR,GPIO.OUT)
+GPIO.setup(M2pwmL,GPIO.OUT)
+GPIO.setup(M2pwmR,GPIO.OUT)
+# Set up the PWM pins so the board will send PWM signals.
+M1pwmL = GPIO.PWM(M1pwmL,1000)  #1000 is the Hz frequency. We can maybe play with this value to change the motor speed.
+M1pwmR = GPIO.PWM(M1pwmR,1000)
+M2pwmL = GPIO.PWM(M2pwmL,1000)
+M2pwmR = GPIO.PWM(M2pwmR,1000)  #NOTES ON CONTROLLING MOTORS WITH PWM SIGNALS
+                                    #pwmL.start(100) will start the motor spinning left at 100% speed
+                                    #pwmL.stop() will stop the motor
+                                    #pwmL.ChangeDutyCycle(50) will set the speed of the motor to 50%
+#######################################################
+############# Sensor Setup ############################
+leftBack = Button(14)
+rightBack = Button(15)
+leftSide = Button(4)
+rightSide = Button(17)
 
-leftButton = 1 ######
-#GPIO Set Up#
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(leftButton, GPIO.IN)
+lineSensor = LineSensor(27)
 
-leftBack = Button(1) ######
-rightBack = Button(2)  #######
-leftSide = Button(3) ######
-rightSide = Button(4) #####
-
-lineSensor = 1		#####		#TRCT5000 is the sensor I think we should use.
-GPIO.setup(lineSensor, GPIO.IN)
-
-ballLeft = DistanceSensor(echo=17, trigger=4, threshold_distance=0.127) ######
-ballRight = DistanceSensor(echo=17, trigger=4, threshold_distance=0.127)  ######
-ballCenter = DistanceSensor(echo=17, trigger=4, threshold_distance=0.127) ######
-startButton = Button(5) #######
-testButton = Button(6) #######
+ballLeft = DistanceSensor(echo=23, trigger=24, threshold_distance=0.127)
+ballRight = DistanceSensor(echo=22, trigger=10, threshold_distance=0.127)
+ballCenter = DistanceSensor(echo=25, trigger=8, threshold_distance=0.127)
+startButton = Button(20)
+testButton = Button(21)
 
 def Test_Driver_Motors():
-	leftDriver = mh.getMotor(1)
-	rightDriver = mh.getMotor(2)
-	print("Motors Set Up")
-	#set speed
-	leftDriver.setSpeed(100)
-	rightDriver.setSpeed(100)
-	print("Speed Set")
-	#run test
-	leftDriver.run(Raspi_MotorHAT.FORWARD)
-	rightDriver.run(Raspi_MotorHAT.FORWARD)
-	print("Moving Forward")
-	#turn off
-	leftDriver.run(Raspi_MotorHAT.RELEASE)
-	rightDriver.run(Raspi_MotorHAT.RELEASE)
-	print("Test Complete")
+	try:
+		print("Starting test.")
+		Drive_Left(50)
+		time.sleep(2)
+		Drive_Stop()
+		time.sleep(2)
+		Drive_Right(75)
+		time.sleep(2)
+		Drive_Stop()
+
+		print("Test Complete.")
+
+	except KeyboardInterrupt:
+		print("Keyboard Interrupt")
+		GPIO.cleanup()
 	
-def Drive_Left():
-	print("Going Left")
-	# open left ramp
-	leftDriver.run(Raspi_MotorHAT.FORWARD)
-	rightDriver.run(Raspi_MotorHAT.FORWARD)
-	leftSide.when_pressed	
+def Drive_Left(speed):
+	GPIO.output(M1enL, GPIO.HIGH)
+	GPIO.output(M1enR, GPIO.HIGH)
+	GPIO.output(M2enL, GPIO.HIGH)
+	GPIO.output(M2enR, GPIO.HIGH)
+	M1pwmL.start(speed)  # starts at a speed of whatever % int you call the function with.
+	M2pwmL.start(speed)
+	print(f"Going Left at {speed}% speed")
 	
-def Drive_Right():
-	print("Going Right")
-	leftDriver.run(Raspi_MotorHAT.BACKWARD)
-	rightDriver.run(Raspi_MotorHAT.BACKWARD)
+def Drive_Right(speed):
+	GPIO.output(M1enL, GPIO.HIGH)
+	GPIO.output(M1enR, GPIO.HIGH)
+	GPIO.output(M2enL, GPIO.HIGH)
+	GPIO.output(M2enR, GPIO.HIGH)
+	M1pwmR.start(speed)  # starts at a speed of whatever % int you call the function with.
+	M2pwmR.start(speed)
+	print(f"Going Right at {speed}% speed")
 	
 def Drive_Stop():
+	M1pwmL.stop()
+	M1pwmR.stop()
+	M2pwmL.stop()
+	M2pwmR.stop()
 	print("Stopped")
-	leftDriver.run(Raspi_MotorHAT.RELEASE)
-	rightDriver.run(Raspi_MotorHAT.RELEASE)
 
 def Balls():		# Might need to run in main so it can run continuously
 	global leftBall, centerBall, rightBall
@@ -100,9 +128,8 @@ def Start_Button():
 def Left_Load():
 	global onLeft, lastWall, status
 	#open left ramp
-	leftDriver.run(Raspi_MotorHAT.FORWARD)
-	rightDriver.run(Raspi_MotorHAT.FORWARD)
-	leftSide.when_pressed = Drive_Stop()
+	Drive_Left(75)
+	leftSide.when_pressed = Drive_Stop()	#This might need to be "wait for press"
 	lastWall = "left"
 	#shut left ramp MUST MOUNT SWITCH SO THAT RAMP IS NOT TOUCHING THE WALL WHEN IT IS PRESSED!!!
 	Balls() # check if balls were loaded
@@ -114,9 +141,8 @@ def Left_Load():
 def Right_Load():
 	global onRight, lastWall, status
 	#open right ramp
-	leftDriver.run(Raspi_MotorHAT.BACKWARD)
-	rightDriver.run(Raspi_MotorHAT.BACKWARD)
-	rightSide.when_pressed = Drive_Stop()
+	Drive_Right(75)
+	rightSide.when_pressed = Drive_Stop()	#This might need to be "wait for press"
 	lastWall = "right"
 	#shut right ramp MUST MOUNT SWITCH SO THAT RAMP IS NOT TOUCHING THE WALL WHEN IT IS PRESSED!!!
 	Balls() # check if balls were loaded
@@ -132,28 +158,27 @@ def On_Center():
 	else:
 		onCenter = False
 				
-def Aiming():
+def Aiming():	#NEED TO CHANGE WITH NEW DRIVE FUNCTIONS AND LINESENSOR FUNCTIONALITY
 	global onCenter, status
 	if lastWall == "right":
 		while True:
-			if GPIO.input(lineSensor):	#not on line
-				leftDriver.run(Raspi_MotorHAT.FORWARD)
-				rightDriver.run(Raspi_MotorHAT.FORWARD)
-			else:
-				leftDriver.run(Raspi_MotorHAT.RELEASE)
-				rightDriver.run(Raspi_MotorHAT.RELEASE)
+			Drive_Left(25)	# Set to 25 so we are driving slower and can stop faster when she finds the line.
+			lineSensor.wait_for_line()
+			Drive_Stop()
+			if lineSensor.active_state:	#not sure if this will actually work the way I am thinking which is that lineSensor.active_state will return True if it is on a line.
 				onCenter = True
 				Take_Shot()			#need to write for now just have motor run continuous?
-				status = pickup
-				break
+				status = "pickup"
+			else:
+				Aiming()
+			break
 	elif lastWall == "left":
 		while True:
-			if GPIO.input(lineSensor):	#not on line
-				leftDriver.run(Raspi_MotorHAT.BACKWARD)
-				rightDriver.run(Raspi_MotorHAT.BACKWARD)
-			else:
-				leftDriver.run(Raspi_MotorHAT.RELEASE)
-				rightDriver.run(Raspi_MotorHAT.RELEASE)
+			Drive_Right(25)
+			lineSensor.wait_for_line()
+			Drive_Stop()
+			if lineSensor.active_state:	# see note above.
+				Drive_Stop()
 				onCenter = True
 				Take_Shot()			#need to write for now just have motor run continuous?
 				status = "pickup"
@@ -220,7 +245,7 @@ ballInCenter = False	# this assumes we do not start with a ball in the bot. I th
 onCenter = False	#Even if we do start with it lined up in the middle this value should be changed in test so we can make sure the line sensors are working.
 
 #Test/Start Buttons
-testButton.when_pressed = testDriverMotors()	#need to add test line sensors to test script.
+testButton.when_pressed = Test_Driver_Motors()	#need to add test line sensors to test script.
 startButton.when_pressed  = Start_Button()
 
 while status != "ready":		# should run until startbutton is pressed again
